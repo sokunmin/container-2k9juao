a210e6987c76713ca75a36fcd48a873542da6148
Chun-Ming <sokunmin@gmail.com>
2023-11-28 23:38:48 +0800

    feat: combine endpoints together

--- src/app/echo.py
+++ src/app/echo.py
@@ -1,20 +1,39 @@
-from fastapi import FastAPI
 from fastapi_poe.types import ProtocolMessage
 from fastapi_poe.client import get_bot_response
 import asyncio
+from datetime import datetime
+from fastapi import FastAPI, Request
 
 app = FastAPI()
+
 
 async def get_responses(api_key, message_content):
     message = ProtocolMessage(role="user", content=message_content)
     responses = []
-    async for partial in get_bot_response(messages=[message], bot_name="GPT-3.5-Turbo", api_key=api_key):
+    async for partial in get_bot_response(
+        messages=[message], bot_name="GPT-3.5-Turbo", api_key=api_key
+    ):
         responses.append(partial)
     return responses
 
 
-@app.post("/api/respond")
+@app.post("/api/echo1")
 async def respond(message_content: str):
     api_key = "ZUxAdhHe0TWI7RxnpIDo4KQakLm79WRr"  # Replace with your actual API key
     responses = await get_responses(api_key, message_content)
     return {"responses": responses}
+
+
+@app.post("/api/echo2")
+async def respond(message_content: str):
+    api_key = "XRsROUqjRsVNvTStZv9Km5CcCr7H8BlK"  # Replace with your actual API key
+    responses = await get_responses(api_key, message_content)
+    return {"responses": responses}
+
+
+@app.post("/api/test")
+async def receive_data(request: Request):
+    data = await request.json()
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print("server.receive=", data)
+    return {"message": now, "success": True}
--- test_client/client.py
+++ test_client/client.py
@@ -1,6 +1,8 @@
 import requests
 from datetime import datetime
 url = "https://container-2k9juao.containers.anotherwebservice.com:5003/api/test"
+# url = "https://container-2k9juao.containers.anotherwebservice.com:5003/api/echo1"
+# url = "https://container-2k9juao.containers.anotherwebservice.com:5003/api/echo2"
 # url = "http://server-container:5003/api/test"
 now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 data = {"send-time": now}
